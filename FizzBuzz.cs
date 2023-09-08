86335d3e36b1d77d2f26fd7cb232d604a95258fd
MaxAim <75140148+MaxAim@users.noreply.github.com>
2023-09-08 13:00:04 -0300

    Add files via upload

---
+++ FizzBuzz.cs
@@ -0,0 +1,15 @@
+﻿Dictionary<int, string> words = new Dictionary<int, string>() {[3] = "Fizz", [5] = "Buzz"};
+
+for (int i = 1; i <= 100; i++)
+{
+    bool empty = true;
+    foreach (var item in words)
+    {
+        if (i % item.Key == 0)
+        {
+            Console.Write(item.Value);
+            empty = false;
+        }
+    }
+    Console.WriteLine(empty ? i : "");
+}
